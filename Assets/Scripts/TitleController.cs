d4b23c1aaac8a6a93e580522176129e259750206
Mari-Hayashi <mhaya010@ucr.edu>
2019-05-18 11:53:58 -0700

    Made scenes

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class TitleController : MonoBehaviour
+{
+    private const float timeToWait = 2f;
+    void Start()
+    {
+        StartCoroutine("ShowTitle");
+    }
+    IEnumerator ShowTitle()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene("Parent");
+    }
+}
